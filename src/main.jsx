facbf5e87fc7310c82aa091c4754f4e15dfcd137
りょーた <ryota2033@gmail.com>
2026-04-28 15:42:26 +0900

    静的HTMLのCSSリンクをViteビルド後のパスに修正

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import RootRouter from './RootRouter';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../components/Header';
-import '../index.css';
 
 export default function Privacy() {
   return (
